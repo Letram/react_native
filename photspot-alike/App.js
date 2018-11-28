d0a6b062a486e948e6eed87e072fa5e5c70d1a69
carlos.martel105@alu.ulpgc.es <carlos.martel105@alu.ulpgc.es>
2018-11-28 20:34:22 +0000

    react camera

---
+++ photspot-alike/App.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera, MapView, Location, Permissions} from 'expo';
+
+export default class App extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    const { locationStatus } = await Permissions.askAsync(Permissions.LOCATION);
+
+    this.setState({ 
+      hasCameraPermission: status === 'granted',
+      hasLocationPermission: locationStatus === 'granted',
+      location: {
+        latitude: 0,
+        longitude: 0
+      }
+   });
+   let posLocation = await Location.getCurrentPositionAsync({});
+   console.log(this.state.location);
+   this.setState({location: {
+      latitude: posLocation.coords.latitude,
+      longitude: posLocation.coords.longitude
+   }});
+   console.log(this.state.location);
+  }
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    const { hasLocationPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera style={{ flex: 1 }} type={this.state.type}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 0.5,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  console.log("yay");
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Take Picture{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+          <MapView
+            style={{ flex: 1 }}
+            initialRegion={{
+              latitude: this.state.location.latitude,
+              longitude: this.state.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  preview: { 
+    flex: 1, 
+    justifyContent: 'flex-end', 
+    alignItems: 'center', 
+  },
+    capture: { 
+      flex: 0, 
+      backgroundColor: '#fff', 
+      borderRadius: 5, 
+      color: '#000', 
+      padding: 10, 
+      margin: 40
+    }
+});
